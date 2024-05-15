ed6582e7177838f0494173145dd9318f0b630468
CodeByOli <oliver.acnh@gmail.com>
2024-05-15 10:26:07 +0200

    Code Extension

--- Pong-Game.py
+++ Pong-Game.py
@@ -12,3 +12,14 @@ WIN = pygame.display.set_mode((infoObject.current_w, infoObject.current_h), pyga
 WIDTH, HEIGHT = WIN.get_width(), WIN.get_height()
  
 pygame.display.set_caption("PONG")
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+ 
+# Game variables
+PADDLE_WIDTH, PADDLE_HEIGHT = 10, 100
+BALL_SIZE = 20
+PADDLE_SPEED = 5
+BALL_INITIAL_SPEED_X = 3
+BALL_INITIAL_SPEED_Y = 3
